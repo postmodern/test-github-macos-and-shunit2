b49fa155b7d69a18fe1527367bc50a2fb5875a5a
Postmodern <postmodern.mod3@gmail.com>
2021-06-04 19:32:16 -0700

    Added more print debugging to determine why the rm in tearDown is returning non-zero.

--- test/file_setup_test.sh
+++ test/file_setup_test.sh
@@ -2,10 +2,25 @@
 
 . ./test/helper.sh
 
+test_fixtures_dir="$PWD/test/fixtures"
+
+
+function print_state()
+{
+	echo -e "PWD:\t\t\t$PWD"
+	echo -e "test_fixtures_dir:\t$test_fixtures_dir"
+	echo -e "test_file:\t\t$test_file"
+	echo "test/fixtures:"
+	ls -a "$test_fixtures_dir"
+}
+
 function setUp()
 {
-	test_file="$PWD/test/fixtures/file.txt"
+	test_file="$test_fixtures_dir/file.txt"
 
+	print_state
+
+	echo ">>> Creating test file $test_file ..."
 	echo "test file" > "$test_file"
 }
 
@@ -18,7 +33,10 @@ function test_file_creation()
 
 function tearDown()
 {
-	rm "$PWD/test/fixtures/file.txt"
+	echo ">>> Removing test file $test_file ..."
+	rm "$test_file"
+
+	print_state
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
